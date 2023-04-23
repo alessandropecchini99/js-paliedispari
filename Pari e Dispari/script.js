d77d8f8c0750a9bcf9055317cf9fca03f5d48f33
alessandropecchini99 <pecchini99@gmail.com>
2023-04-23 22:43:40 +0200

    fix pari/dispari & add simple interface

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -8,22 +8,57 @@
 
 
 
-// 1. CHIEDERE PARI/DISPARI E UN NUMERO DA 1 A 5
-const disPari = prompt(`Scegli Pari o Dispari`).toLowerCase();
-const userNum = parseInt(prompt(`inserisci un numero da 1 a 5`));
-console.log(disPari);
-console.log(userNum);
+// VARIABILI
+const inputWord = document.getElementById(`word`);
+const inputNum = document.getElementById(`number`);
+const btnPlay = document.getElementById(`btn-play`);
+const result = document.getElementById(`result`);
 
-// 2. GENERO CPU NUMBER
-const cpuNum = (Math.floor(Math.random() * 5) + 1);
-console.log(cpuNum);
+// btn-play
+btnPlay.addEventListener(`click`,
+    function() {
+        
+        // 1. CHIEDERE PARI/DISPARI E UN NUMERO DA 1 A 5
+        const disPari = inputWord.value.toLowerCase();
+        const userNum = parseInt(inputNum.value);
+        console.log(disPari);
+        console.log(userNum);
 
-// 3. SOMMO I VALORI
-const somma = (userNum + cpuNum);
-console.log(somma);
+        // 2. GENERO CPU NUMBER
+        const randomNum = randomNumber(1, 5);
+        console.log(randomNum);
+        
+        // 3. SOMMO I VALORI
+        const somma = (userNum + randomNum);
+        console.log(somma);
+        
+        // 4. CONTROLLO CHI HA VINTO
+        const check = controllo(somma)
+        console.log(check);
+        
+        // 5. CONTROLLO VINCITA
+        // if (disPari == check && userNum == randomNum) {
+        if (disPari == controllo(somma)) {
+            document.getElementById(`result`).innerHTML=`Hai Vinto!`;
+            console.log(`Hai Vinto!`)
+        } else {
+            document.getElementById(`result`).innerHTML=`Hai Perso!`;
+            console.log(`Hai Perso!`)
+        }
 
-// 4. CONTROLLO CHI HA VINTO
-const check = controllo(somma)
+    }
+)
+
+
+// ------------------------------
+
+
+// ALTRO
+function randomNumber(min, max) {
+    const random = (Math.floor(Math.random() * max) + min);
+    return random
+}
+
 function controllo(check){
     if (check % 2 == 0) {
         return `pari`;
@@ -32,20 +67,6 @@ function controllo(check){
     }
 }
 
-console.log(check);
-
-// 5. CONTROLLO VINCITA
-if (disPari == check && userNum == cpuNum) {
-    console.log(`Hai Vinto!`)
-} else {
-    console.log(`Hai Perso!`)
-}
-
-
-// ------------------------------
-
-
-// ALTRO
 function refreshPage(){
     window.location.reload();
 } 
